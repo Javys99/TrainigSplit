202c12650eacaedc54145db0c61c1840d3bfa24b
Cristopher Soobin Costales <105178704+CristopherCostales@users.noreply.github.com>
2023-12-29 18:35:33 -0600

    feat: apellidos

--- resources/views/plan/info.blade.php
+++ resources/views/plan/info.blade.php
@@ -1,5 +1,5 @@
 {{$plan -> id}}
-{{$plan -> user -> nombre}}
+{{$plan -> user -> nombre}} {{$plan -> user -> apellido_paterno}} {{$plan -> user -> apellido_materno}}
 {!!$plan -> plan!!}
 {{$plan -> created_at}}
 {{$plan -> updated_at}}
