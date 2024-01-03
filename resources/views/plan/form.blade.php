667a8db9153e615cbf832ff157e9aa74ac8fbcc3
Javys99 <124941186+Javys99@users.noreply.github.com>
2024-01-02 22:09:44 -0600

    text

--- resources/views/plan/form.blade.php
+++ resources/views/plan/form.blade.php
@@ -1,17 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-<form action=" {{ url('/plan/form') }} " method="post" enctype="multipart/form-data">
-@csrf
-<textarea name="plan" value="{{ isset($plan->plan)?$plan->plan:old('plan') }}" id="plan" cols="30" rows="10"></textarea>
-<br>
 
-<input type="submit" value="Guardar" id="guardar">
-</form>
+<body>
+    <form action=" {{ url('/plan/form') }} " method="post" enctype="multipart/form-data">
+        @csrf
+        <textarea name="plan" value="{{ isset($plan->plan) ? $plan->plan : old('plan') }}" id="plan" cols="30"
+            rows="10"></textarea>
+        <br>
+
+        <input type="submit" value="Guardar" id="guardar">
+    </form>
 </body>
+
 </html>
